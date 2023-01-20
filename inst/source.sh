e31f6f14ef6501cfa6d9f118f16a5681cec28b5e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-01-20 10:08:39 -0800

    Updated the schemas.

--- inst/source.sh
+++ inst/source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L https://artifactdb.github.io/CollaboratorDB-schemas/bundle.tar.gz > bundle.tar.gz
+curl -L https://collaboratordb.github.io/CollaboratorDB-schemas/bundle.tar.gz > bundle.tar.gz
 rm -rf schemas
 tar -xvf bundle.tar.gz
 mv resolved schemas
